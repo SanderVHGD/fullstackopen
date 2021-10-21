8be8d685a999f76590b398d670e0145c5c7385e9
SanderVHGD <sander.vhgd@gmail.com>
2021-10-21 16:49:47 +0200

    ex 1.6 - 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({clickHandler, text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={clickHandler}>
+      {text}
+    </button>
+  )
 }
 
-export default App;
+const StatisticLine = ({text, amount}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{amount}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if(good===0 && neutral===0 && bad===0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  const average = () => ((good-bad) / (good+neutral+bad)).toFixed(2)
+  const positive = () => ((good*100) / (good+neutral+bad)).toFixed(2) + " %"
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" amount={good} />
+        <StatisticLine text="neutral" amount={neutral} />
+        <StatisticLine text="bad" amount={bad} />
+        <StatisticLine text="average" amount={average()} />
+        <StatisticLine text="positive" amount={positive()} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button clickHandler={() => setGood(good + 1)} text="good" />
+      <Button clickHandler={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button clickHandler={() => setBad(bad + 1)} text="bad" />
+
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
